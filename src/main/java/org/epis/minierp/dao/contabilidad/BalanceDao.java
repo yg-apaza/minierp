a22dcfc5b82d2d9201dc7f43987977583a2ed42e
Carolina <Carolina@Carolina-PC>
2016-10-05 18:30:31 -0500

    Dao and Dto Balance

--- src/main/java/org/epis/minierp/dao/contabilidad/BalanceDao.java
+++ src/main/java/org/epis/minierp/dao/contabilidad/BalanceDao.java
@@ -1,5 +1,13 @@
 package org.epis.minierp.dao.contabilidad;
 
+import org.epis.minierp.util.HibernateUtil;
+import org.hibernate.Session;
+
 public class BalanceDao {
+    private Session session;
     
+    public BalanceDao()
+    {  
+        session = HibernateUtil.getSessionFactory().getCurrentSession();  
+    }
 }
