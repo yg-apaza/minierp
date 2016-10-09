625cb0936e3798b7255da83a0f6b1b31fa72efc9
gladyschy5 <gladys.chy5@gmail.com>
2016-10-09 11:50:08 -0500

    VISTA_CAJA_BANCOS
    Conflicts:
    	src/main/resources/hibernate.cfg.xml

--- src/main/java/org/epis/minierp/dao/contabilidad/cajaBancos/BancosModel.java
+++ src/main/java/org/epis/minierp/dao/contabilidad/cajaBancos/BancosModel.java
@@ -1,10 +1,17 @@
 package org.epis.minierp.dao.contabilidad.cajaBancos;
 
+import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
 
-public class BancosModel {
+@Entity
+@IdClass(CajaPK.class)
+@Table(name="VIEW_BANCOS")
+public class BancosModel implements Serializable {
     @Id 
     @Column(name="LibDiaCod")
     private String libDiaCod;
@@ -20,8 +27,8 @@ public class BancosModel {
     @Column(name="AsiCabFec")
     private Date asiCabFec;
     
-    @Column(name="AsiCabTip")
-    private String asiCabTip;
+    @Column(name="CueBanNum")
+    private String cueBanNum;
     
     @Column(name="CueCod")
     private String cueCod;
@@ -34,4 +41,78 @@ public class BancosModel {
     
     @Column(name="Haber")
     private String haber;
+
+    public String getLibDiaCod() {
+        return libDiaCod;
+    }
+
+    public void setLibDiaCod(String libDiaCod) {
+        this.libDiaCod = libDiaCod;
+    }
+
+    public int getAsiDetCod() {
+        return asiDetCod;
+    }
+
+    public void setAsiDetCod(int asiDetCod) {
+        this.asiDetCod = asiDetCod;
+    }
+
+    public int getAsiCabCod() {
+        return asiCabCod;
+    }
+
+    public void setAsiCabCod(int asiCabCod) {
+        this.asiCabCod = asiCabCod;
+    }
+
+    public Date getAsiCabFec() {
+        return asiCabFec;
+    }
+
+    public void setAsiCabFec(Date asiCabFec) {
+        this.asiCabFec = asiCabFec;
+    }
+
+    public String getCueBanNum() {
+        return cueBanNum;
+    }
+
+    public void setCueBanNum(String cueBanNum) {
+        this.cueBanNum = cueBanNum;
+    }
+
+    public String getCueCod() {
+        return cueCod;
+    }
+
+    public void setCueCod(String cueCod) {
+        this.cueCod = cueCod;
+    }
+
+    public String getCueDes() {
+        return cueDes;
+    }
+
+    public void setCueDes(String cueDes) {
+        this.cueDes = cueDes;
+    }
+
+    public String getDebe() {
+        return debe;
+    }
+
+    public void setDebe(String debe) {
+        this.debe = debe;
+    }
+
+    public String getHaber() {
+        return haber;
+    }
+
+    public void setHaber(String haber) {
+        this.haber = haber;
+    }
+    
+    
 }
---
+++ src/main/java/org/epis/minierp/dao/contabilidad/cajaBancos/BancosPK.java
@@ -0,0 +1,68 @@
+package org.epis.minierp.dao.contabilidad.cajaBancos;
+
+import java.io.Serializable;
+
+public class BancosPK implements Serializable {
+    protected String libDiaCod;
+    protected int asiCabCod;
+    protected int asiDetCod;
+    
+    public BancosPK(){}
+
+    public BancosPK(String LibDiaCod, int AsiCabCod, int AsiDetCod) {
+        this.libDiaCod = LibDiaCod;
+        this.asiCabCod = AsiCabCod;
+        this.asiDetCod = AsiDetCod;
+    }
+
+    public String getLibDiaCod() {
+        return libDiaCod;
+    }
+
+    public void setLibDiaCod(String LibDiaCod) {
+        this.libDiaCod = LibDiaCod;
+    }
+
+    public int getAsiCabCod() {
+        return asiCabCod;
+    }
+
+    public void setAsiCabCod(int AsiCabCod) {
+        this.asiCabCod = AsiCabCod;
+    }
+
+    public int getAsiDetCod() {
+        return asiDetCod;
+    }
+
+    public void setAsiDetCod(int AsiDetCod) {
+        this.asiDetCod = AsiDetCod;
+    }
+    
+    public boolean equals(Object other) {
+        if ((this == other)) {
+            return true;
+        }
+        if ((other == null)) {
+            return false;
+        }
+        if (!(other instanceof CajaPK)) {
+            return false;
+        }
+        CajaPK castOther = (CajaPK) other;
+
+        return (this.getAsiDetCod() == castOther.getAsiDetCod())
+                && (this.getAsiCabCod() == castOther.getAsiCabCod())
+                && ((this.getLibDiaCod() == castOther.getLibDiaCod()) || (this.getLibDiaCod() != null && castOther.getLibDiaCod() != null && this.getLibDiaCod().equals(castOther.getLibDiaCod())));
+    }
+
+    public int hashCode() {
+        int result = 17;
+
+        result = 37 * result + this.getAsiDetCod();
+        result = 37 * result + this.getAsiCabCod();
+        result = 37 * result + (getLibDiaCod() == null ? 0 : this.getLibDiaCod().hashCode());
+        return result;
+    }
+
+}
