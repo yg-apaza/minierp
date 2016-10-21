4703c2c12134053b9a54432c4bf0fa6e029f0482
PaulaAsto <carolinaasto@gmail.com>
2016-10-21 10:47:56 -0500

    Avance panel compra

--- src/main/java/org/epis/minierp/controller/general/PanelController.java
+++ src/main/java/org/epis/minierp/controller/general/PanelController.java
@@ -1,7 +1,11 @@
 package org.epis.minierp.controller.general;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +15,8 @@ import javax.servlet.http.HttpSession;
 import org.epis.minierp.dao.ventas.IngresosDao;
 import org.epis.minierp.dao.ventas.PreventasDao;
 import org.epis.minierp.model.EnP1mUsuario;
+import org.epis.minierp.model.EnP2mAlmacen;
+import org.epis.minierp.model.EnP2mProducto;
 import org.epis.minierp.model.Ingresos;
 import org.epis.minierp.model.Preventas;
 
@@ -21,38 +27,65 @@ public class PanelController extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        HttpServletRequest req = (HttpServletRequest) request;
         HttpSession session = request.getSession(false);
         EnP1mUsuario u = (EnP1mUsuario) session.getAttribute("usuario");      
+        int type = u.getTaGzzTipoUsuario().getTipUsuCod();
+        
         IngresosDao daoIng = new IngresosDao();
         PreventasDao daoPre = new PreventasDao();
 
         List<Ingresos> ingresos = daoIng.getAll(""+u.getTaGzzTipoUsuario().getTipUsuCod());
         List<Preventas> preventas = daoPre.getAll(""+u.getTaGzzTipoUsuario().getTipUsuCod());
         
-        if(ingresos.size()>0){       
-            Ingresos i = ingresos.get(0);
-            request.setAttribute("iHOY", i.getHOY());
-            request.setAttribute("iAYER", i.getAYER());
-            request.setAttribute("iANTEAYER", i.getANTEAYER());
+        switch(type){
+            case 1:
+                if(ingresos.size()>0){       
+                    Ingresos i = ingresos.get(0);
+                    request.setAttribute("iHOY", i.getHOY());
+                    request.setAttribute("iAYER", i.getAYER());
+                    request.setAttribute("iANTEAYER", i.getANTEAYER());
+                }
+                else{
+                    request.setAttribute("iHOY", 0);
+                    request.setAttribute("iAYER", 0);
+                    request.setAttribute("iANTEAYER", 0);
+                }
+
+                if(preventas.size()>0){       
+                    Preventas p = preventas.get(0);
+                    request.setAttribute("pHOY", p.getHOY());
+                    request.setAttribute("pAYER", p.getAYER());
+                    request.setAttribute("pANTEAYER", p.getANTEAYER());
+                }
+                else{
+                    request.setAttribute("pHOY", 0);
+                    request.setAttribute("pAYER", 0);
+                    request.setAttribute("pANTEAYER", 0);
+                }
+                break;
+            case 2:
+                break;
+            case 3:
+                Iterator <EnP2mAlmacen> stores = u.getEnP1mSucursal().getEnP2mAlmacens().iterator();
+                Set <EnP2mProducto> productosSet = new HashSet <> ();
+                while(stores.hasNext()) {
+                    Iterator <EnP2mProducto> products = stores.next().getEnP2mProductos().iterator();
+                    while(products.hasNext()) {
+                        EnP2mProducto product = products.next();
+                        System.out.println("Producto Almacen " + product.getEnP2mAlmacen().getAlmCod());
+                        if(product.getEstRegCod() == 'A')
+                            productosSet.add(product);
+                    }            
+                }
+                List <EnP2mAlmacen> almacenes = new ArrayList<>(u.getEnP1mSucursal().getEnP2mAlmacens());
+                List <EnP2mProducto> productos = new ArrayList<>(productosSet);
+                request.setAttribute("almacenes", almacenes);
+                request.setAttribute("productos", productos);
+                break;
+            case 4:
+                break;
         }
-        else{
-            request.setAttribute("iHOY", 0);
-            request.setAttribute("iAYER", 0);
-            request.setAttribute("iANTEAYER", 0);
-        }
-        
-        if(preventas.size()>0){       
-            Preventas p = preventas.get(0);
-            request.setAttribute("pHOY", p.getHOY());
-            request.setAttribute("pAYER", p.getAYER());
-            request.setAttribute("pANTEAYER", p.getANTEAYER());
-        }
-        else{
-            request.setAttribute("pHOY", 0);
-            request.setAttribute("pAYER", 0);
-            request.setAttribute("pANTEAYER", 0);
-        } 
+         
         
         request.getRequestDispatcher("/WEB-INF/general/panel.jsp").forward(request, response);
     }
