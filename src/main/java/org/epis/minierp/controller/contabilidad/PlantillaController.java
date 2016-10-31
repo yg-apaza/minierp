09de57af99d6ba2c966630d3ad0db51849df5c61
Yuliana Apaza <yuliana.apaza94@gmail.com>
2016-10-30 21:51:45 -0500

    Controller Plantilla

--- src/main/java/org/epis/minierp/controller/contabilidad/PlantillaController.java
+++ src/main/java/org/epis/minierp/controller/contabilidad/PlantillaController.java
@@ -15,4 +15,42 @@ public class PlantillaController extends HttpServlet
     {
         request.getRequestDispatcher("/WEB-INF/contabilidad/plantilla.jsp").forward(request, response);
     }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String action = request.getParameter("accion");
+        switch(action) {
+            case "create":
+                String plaDet = request.getParameter("plaDet");
+                String plaGlo = request.getParameter("plaGlo");
+                String[] cueNum = request.getParameterValues("cueNum");
+                String[] plaDetDebHab = request.getParameterValues("plaDetDebHab");
+                String[] plaDetPor = request.getParameterValues("plaDetPor");
+                
+                /*
+                System.out.println("-------------------------------------------------");
+                System.out.println("CABECERA DE PLANTILLA");
+                System.out.println("-------------------------------------------------");
+                System.out.println("plaDet: " + plaDet);
+                System.out.println("plaGlo: " + plaGlo);
+                
+                System.out.println("-------------------------------------------------");
+                System.out.println("DETALLE DE PLANTILLA");
+                
+                for(int i = 0; i < cueNum.length; i++)
+                {
+                    System.out.println("-------------------------------------------------");
+                    System.out.println("cueNum: " + cueNum[i] + "\t");
+                    System.out.println("plaDetDebHab: " + plaDetDebHab[i] + "\t");
+                    System.out.println("plaDetPor: " + plaDetPor[i] + "\t");
+                }
+                System.out.println("-------------------------------------------------");
+                */
+                break;
+            case "delete":
+                break;
+        }
+        response.sendRedirect(request.getContextPath() + "/secured/contabilidad/plantilla");
+    }
 }
