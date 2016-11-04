823c484fe1f0438efa2065470c609a58a3f48674
Gonzalo <glunaluza@gmail.com>
2016-11-04 01:15:20 -0500

    Proveedor reporte working again

--- src/main/java/org/epis/minierp/business/general/Impresora.java
+++ src/main/java/org/epis/minierp/business/general/Impresora.java
@@ -3,7 +3,6 @@ package org.epis.minierp.business.general;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
