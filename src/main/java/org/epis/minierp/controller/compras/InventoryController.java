9b4bbb31303b5fbb2f2929c6b67d6b926c1a4766
Diego <Diego@Diego-PC>
2016-11-08 16:29:17 -0500

    "Inventario modificar stock"

--- src/main/java/org/epis/minierp/controller/compras/InventoryController.java
+++ src/main/java/org/epis/minierp/controller/compras/InventoryController.java
@@ -6,9 +6,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.epis.minierp.business.logistica.EnP2mProductoBusiness;
+import org.epis.minierp.dao.general.TaGzzTipoFallaProductoDao;
 import org.epis.minierp.dao.logistica.EnP2mProductoDao;
 import org.epis.minierp.model.EnP2mProducto;
-import org.epis.minierp.business.logistica.EnP2mProductoBusiness;
+import org.epis.minierp.model.TaGzzTipoFallaProducto;
 
 public class InventoryController extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -18,8 +20,12 @@ public class InventoryController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {       
         
         EnP2mProductoDao daoPro = new EnP2mProductoDao();
-        List<EnP2mProducto> productos = daoPro.getAll();
+        List<EnP2mProducto> productos = daoPro.getAllActive();
         request.setAttribute("productos", productos);
+        
+        TaGzzTipoFallaProductoDao daoFalla = new TaGzzTipoFallaProductoDao();
+        List<TaGzzTipoFallaProducto> falla_producto = daoFalla.getAll();
+         request.setAttribute("falla_producto", falla_producto);
        /* UsuarioDao daoUsu = new UsuarioDao();
         List<EnP1mUsuario> usuarios = daoUsu.getAll();
         request.setAttribute("usuarios", usuarios);*/
