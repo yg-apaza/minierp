398d3a335da04c43367c267003290857f5df0216
Carolina <Carolina@Carolina-PC>
2016-11-25 21:35:03 -0500

    LibroMayor :v

--- src/main/java/org/epis/minierp/controller/contabilidad/LibroMayorController.java
+++ src/main/java/org/epis/minierp/controller/contabilidad/LibroMayorController.java
@@ -7,19 +7,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.epis.minierp.business.contabilidad.LibroMayorBusiness;
 import org.epis.minierp.dao.contabilidad.CuentaDao;
+import org.epis.minierp.dao.contabilidad.LibroDiarioDao;
 import org.epis.minierp.dao.contabilidad.LibroDiarioViewDao;
 import org.epis.minierp.dao.general.EnP1mEmpresaDao;
+import org.epis.minierp.model.EnP3mLibroDiario;
 
 
 public class LibroMayorController extends HttpServlet{
     private static final long serialVersionUID = 1L;
     String codigo_cuenta ="10";
+    EnP3mLibroDiario libros = null;
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LibroDiarioViewDao libroDiarioDAO = new LibroDiarioViewDao();
         
         EnP1mEmpresaDao empDao = new EnP1mEmpresaDao();
+        try{libros = (new LibroDiarioDao()).getAll().get(0);}catch(Exception e){}
+        request.setAttribute("libros",libros);
         request.setAttribute("empresa", empDao.getAll().get(0));
         request.setAttribute("cuenta", new CuentaDao().getByNumNivActive(codigo_cuenta,2));
         request.setAttribute("operaciones",libroDiarioDAO.getAllNiv(codigo_cuenta));
